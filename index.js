ea0c028f78e80b2779055ea9953ad89d68d75dfd
PritSapariya <pritsapariya17@gmail.com>
2021-04-12 17:36:41 +0530

    Initial Router setup done

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+//Middlewares
+app.use(cors());
+app.use(express.json());
 
+//Importing Routes
+const authRouter = require('./routers/auth');
 
+//Setting Routes
+app.use('/api/user', authRouter);
 
 // Listing on port
 app.listen(3000, () => console.log('Server is running'));
